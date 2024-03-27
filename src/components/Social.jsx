335513f42ffe31d134cbc61b348354b9da824464
hozayves <muhozayves2014@gmail.com>
2024-03-27 10:08:20 +0200

    fx add transition on button hover

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Social({children}) {
   return (
-    <button className='p-3 rounded-md text-white bg-grey w-full text-base sm:text-sm font-medium hover:cursor-pointer hover:text-offBlack hover:bg-green'>
+    <button className='p-3 rounded-md text-white bg-grey w-full text-base sm:text-sm font-medium hover:cursor-pointer hover:text-offBlack hover:bg-green transition-all duration-500'>
         {children}
     </button>
   )
